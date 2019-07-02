3e064ae9b37d11efba27c47e1523ad19efebed97
raechel <odom.raechel@gmail.com>
2019-07-02 15:11:46 -0600

    current endpoints working, need to return id from palette post

--- data.js
+++ data.js
@@ -1,3 +1,14 @@
-const projectsData = [{name: 'My First Project'}]
+const projectsData = [{
+  name: 'My First Project',
+  palettes: [{
+    name: 'My first Palette',
+    color_1: '#FFD289',
+    color_2: '#9B8816',
+    color_3: '#9C6103',
+    color_4: '#F98948',
+    color_5: '#97CC04',
+    color_6: '#729EA1'
+  }]
+}]
 
 module.exports = projectsData
