645735790b5cf7e5b5ac47ba8e2957c7c97d5213
raechel <odom.raechel@gmail.com>
2019-07-04 14:49:39 -0600

    get tests update to pass, get single project pass, post single project pass

--- server.test.js
+++ server.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const app = require('./server.js');
-const bodyParser = require("body-parser")
+// const bodyParser = require("body-parser")
 const environment = process.env.NODE_ENV || 'test';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
@@ -8,35 +8,55 @@ const database = require('knex')(configuration);
 
 describe('Server', () => {
 
-app.use(bodyParser.json())
 
   beforeEach(async() => {
     await database.seed.run()
-    app.use(bodyParser.json())
+    // app.use(bodyParser.json())
   })
 
- describe('GET /palettes', () => {
+  describe('GET /palettes', () => {
 
-  it('should return all the palettes in the database', async () => {
-    const expectedPalettes = await database('palettes').select();
-    // console.log('exptected palettes', expectedPalettes)
-    const res = await request(app).get('/api/v1/palettes/');
-    console.log('res.body', res.body)
-    const palettes = res.body;
-    
-    expect(palettes).toEqual(expectedPalettes);
-  });
+    it('should return all the palettes in the database', async () => {
+      const expectedPalettes = await database('palettes').select();
+      const res = await request(app).get('/api/v1/palettes');
+      const palettes = res.body;
+      
+      expect(palettes).toEqual(expectedPalettes);
+    });
  });
 
- describe('GET /projects', () => {
+  describe('GET /projects', () => {
 
-  it('should return all the projects in the database', async () => {
-    const expectedProjects = await database('projects').select();
-    const res = await request(app).get('/api/v1/projects');
-    const projects = res.body;
+    it('should return all the projects in the database', async () => {
+      const expectedProjects = await database('projects').select();
+      const res = await request(app).get('/api/v1/projects');
+      const projects = res.body.name;
 
-    expect(projects).toEqual(expectedProjects);
-  });
+      expect(projects).toEqual(expectedProjects.name);
+    });
  });
+
+  describe('GET /projects/:id', () => { 
+
+    it('should get a single project from the database', async () => {
+      const expectedProject = await database('projects').first() 
+      const id = expectedProject.id
+      const res = await request(app).get(`/api/v1/projects/${id}`)
+      const result = res.body[0].name
+      expect(result).toEqual(expectedProject.name);
+      });
+  });
+
+  describe('POST /projects', () => {
+
+    it('should post a new project to the database', async () => {
+      const newProject = { name: 'sweets project' }
+      const res = await request(app).post('/api/v1/projects').send(newProject)
+      const projects = await database('projects').where('id', res.body.id).select()
+      const project = projects[0]
+      expect(res.status).toBe(201)
+      expect(project.name).toEqual(newProject.name)
+    });
+  });
  
 });
