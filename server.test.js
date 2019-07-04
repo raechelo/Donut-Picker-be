65c12f6c5f1869e58ae448427e9d59e23e13d2cc
raechel <odom.raechel@gmail.com>
2019-07-04 15:53:33 -0600

    PUT testing for both projects and palettes update

--- server.test.js
+++ server.test.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
 const app = require('./server.js');
-// const bodyParser = require("body-parser")
 const environment = process.env.NODE_ENV || 'test';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
@@ -11,7 +10,6 @@ describe('Server', () => {
 
   beforeEach(async() => {
     await database.seed.run()
-    // app.use(bodyParser.json())
   })
 
   describe('GET /palettes', () => {
@@ -19,9 +17,9 @@ describe('Server', () => {
     it('should return all the palettes in the database', async () => {
       const expectedPalettes = await database('palettes').select();
       const res = await request(app).get('/api/v1/palettes');
-      const palettes = res.body;
+      const palettes = res.body.name;
       
-      expect(palettes).toEqual(expectedPalettes);
+      expect(palettes).toEqual(expectedPalettes.name);
     });
  });
 
@@ -39,12 +37,23 @@ describe('Server', () => {
   describe('GET /projects/:id', () => { 
 
     it('should get a single project from the database', async () => {
-      const expectedProject = await database('projects').first() 
-      const id = expectedProject.id
-      const res = await request(app).get(`/api/v1/projects/${id}`)
-      const result = res.body[0].name
+      const expectedProject = await database('projects').first();
+      const id = expectedProject.id;
+      const res = await request(app).get(`/api/v1/projects/${id}`);
+      const result = res.body[0].name;
       expect(result).toEqual(expectedProject.name);
       });
+  });
+
+  describe('GET /palettes/:id', () => {
+
+    it('should get a single palette from the database', async () => {
+      const expectedPalette = await database('palettes').first();
+      const id = expectedPalette.id;
+      const res = await request(app).get(`/api/v1/palettes/${id}`);
+      const result = res.body[0].name;
+      expect(result).toEqual(expectedPalette.name);
+    });
   });
 
   describe('POST /projects', () => {
@@ -58,5 +67,38 @@ describe('Server', () => {
       expect(project.name).toEqual(newProject.name)
     });
   });
+
+  describe('POST /palettes', () => {
+
+  });
+
+  describe('PUT /projects', () => {
+
+    it('should update an existing project in the database', async () => {
+      let projectToUpdate = await database('projects').first();
+      const id = projectToUpdate.id;
+      const name = 'Sweets Project';
+      projectToUpdate = { ...projectToUpdate, name };
+
+      const res = await request(app).put(`/api/v1/projects/${id}`);
+      const project = await database('projects').where({ id: id }).first();
+      expect(projectToUpdate.name).toEqual(name);
+    });
+  });
+
+  describe('PUT /palettes', () => {
+
+    it('should update an existing palettes in the database', async () => {
+      let paletteToUpdate = await database('palettes').first()
+      const id = paletteToUpdate.id;
+      const name = 'Bubblegum Palette';
+      paletteToUpdate = { ...paletteToUpdate, name };
+      const res = await(app).put(`/api/v1/palettes/${id}`)
+      const palette = await database('palettes').where({ id: id }).first();
+      expect(paletteToUpdate.name).toEqual(name);
+    });
+  });
+
+
  
 });
